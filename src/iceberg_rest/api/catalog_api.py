b386ec9dcf7b99c864586962ee7918bcac96514c
quy do <quy196hp@gmail.com>
2024-07-26 14:17:36 +0700

    update libraries

--- src/iceberg_rest/api/catalog_api.py
+++ src/iceberg_rest/api/catalog_api.py
@@ -66,7 +66,19 @@ def get_config(
         alias="warehouse",
     ),
 ) -> CatalogConfig:
-    """All REST clients should first call this route to get catalog configuration properties from the server to configure the catalog and its HTTP client. Configuration from the server consists of two sets of key/value pairs. - defaults -  properties that should be used as default configuration; applied before client configuration - overrides - properties that should be used to override client configuration; applied after defaults and client configuration  Catalog configuration is constructed by setting the defaults, then client- provided configuration, and finally overrides. The final property set is then used to configure the catalog.  For example, a default configuration property might set the size of the client pool, which can be replaced with a client-specific setting. An override might be used to set the warehouse location, which is stored on the server rather than in client configuration.  Common catalog configuration settings are documented at https://iceberg.apache.org/docs/latest/configuration/#catalog-properties"""
+    """All REST clients should first call this route to get catalog configuration properties from the server
+    to configure the catalog and its HTTP client.
+     Configuration from the server consists of two sets
+     of key/value pairs. - defaults -  properties that should be used as default configuration;
+     applied before client configuration - overrides - properties that should be used to override client
+     configuration; applied after defaults and client configuration
+     Catalog configuration is constructed by setting the defaults, then client- provided configuration,
+     and finally overrides. The final property set is then used to configure the catalog.
+     For example, a default configuration property might set the size of the client pool,
+     which can be replaced with a client-specific setting. An override might be used to set
+     the warehouse location, which is stored on the server rather than in client configuration.
+     Common catalog configuration settings are documented at
+     https://iceberg.apache.org/docs/latest/configuration/#catalog-properties"""
     return CatalogConfig(overrides={}, defaults={})
 
 
